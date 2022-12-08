3b5ba68a0c02015d41c5d6b824b976acfc3dee4b
BuildTools <unconfigured@null.spigotmc.org>
2022-12-07 20:05:43 -0500

    glassbox distant lines and back rectangle

--- public/glassbox/glassbox.js
+++ public/glassbox/glassbox.js
@@ -15,16 +15,77 @@ function setCanvasSize() {
 }
 
 function getCursorPosition(event) {
-    let xPos = event.clientX;
-    let yPos = event.clientY;
-    let xOffset = (xPos - canvas.width / 2) / 10
-    let yOffset = (yPos - canvas.height / 2) / 10
+    let posX = event.clientX
+    let posY = event.clientY
+    let xOffset = (posX - canvas.width / 2) / 6
+    let yOffset = (posY - canvas.height / 2) / 6
+    let centerX = (canvas.width / 2) - xOffset
+    let centerY = (((canvas.height / 2) * 1.3) - yOffset)
 
-    ctx.clearRect(0, 0, canvas.width, canvas.height)
-    ctx.fillStyle = '#0074d9'
+    let dColor = '#dfd3d3'
+    let lColor = '#fff5f5'
+
+    ctx.beginPath();
+    ctx.rect(0, 0, canvas.width, canvas.height)
+    ctx.fillStyle = lColor
+    ctx.fill();
+
+    ctx.lineWidth = 6
+
+    //4 lines towards center
     ctx.beginPath()
     ctx.moveTo(0, 0)
-    ctx.lineTo((canvas.width / 2) - xOffset, (canvas.height / 2) - yOffset)
-    ctx.lineTo(canvas.width, 0)
-    ctx.fill()
+    ctx.lineTo(centerX, centerY)
+    let grd = ctx.createLinearGradient(0, 0, centerX, centerY)
+    grd.addColorStop(0, dColor)
+    grd.addColorStop(0.5, dColor)
+    grd.addColorStop(0.8, lColor)
+    grd.addColorStop(1, lColor)
+    ctx.strokeStyle = grd
+    ctx.stroke()
+
+    ctx.beginPath()
+    ctx.moveTo(canvas.width, 0)
+    ctx.lineTo(centerX, centerY)
+    grd = ctx.createLinearGradient(canvas.width, 0, centerX, centerY)
+    grd.addColorStop(0, dColor)
+    grd.addColorStop(0.5, dColor)
+    grd.addColorStop(0.8, lColor)
+    grd.addColorStop(1, lColor)
+    ctx.strokeStyle = grd
+    ctx.stroke()
+
+    ctx.beginPath()
+    ctx.moveTo(0, canvas.height)
+    ctx.lineTo(centerX, centerY)
+    grd = ctx.createLinearGradient(0, canvas.height, centerX, centerY)
+    grd.addColorStop(0, dColor)
+    grd.addColorStop(0.5, dColor)
+    grd.addColorStop(0.8, lColor)
+    grd.addColorStop(1, lColor)
+    ctx.strokeStyle = grd
+    ctx.stroke()
+
+    ctx.beginPath()
+    ctx.moveTo(canvas.width, canvas.height)
+    ctx.lineTo(centerX, centerY)
+    grd = ctx.createLinearGradient(canvas.width, canvas.height, centerX, centerY)
+    grd.addColorStop(0, dColor)
+    grd.addColorStop(0.5, dColor)
+    grd.addColorStop(0.8, lColor)
+    grd.addColorStop(1, lColor)
+    ctx.strokeStyle = grd
+    ctx.stroke()
+
+    //back rectangle
+    let dist = 0.15
+    ctx.beginPath()
+    ctx.moveTo(centerX * dist, centerY * dist)
+    ctx.lineTo((canvas.width * (2 - dist) / 2) - (xOffset * dist), centerY * dist)
+    ctx.lineTo((canvas.width * (2 - dist) / 2) - (xOffset * dist), ((canvas.height * (2 - dist) / 2) * 1.025) - (yOffset * dist))
+    ctx.lineTo(centerX * dist, ((canvas.height * (2 - dist) / 2) * 1.025) - (yOffset * dist))
+    ctx.closePath()
+    ctx.strokeStyle = dColor
+    ctx.lineWidth = 5
+    ctx.stroke()
 }
